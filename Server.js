1249f2e27618eaa3b226f2b564b3817d9b8a1534
saif-lesnar <saif_lesnar@outlook.com>
2018-06-28 17:52:42 +0600

    Express JS and Https initialized

---
+++ Server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+var fs = require('fs');
+var key = fs.readFileSync('encryption/private.key');
+var cert = fs.readFileSync( 'encryption/primary.crt' );
+var ca = fs.readFileSync( 'encryption/intermediate.crt' );
+var https = require('https');
+const bodyParser = require('body-parser');
+var options = {
+key: key,
+cert: cert,
+ca: ca
+};
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', function (req, res) {
+  console.log('working');
+  res.send('Chatbot website')
+})
+
+https.createServer(options, app).listen(3000);
+console.log('Server started!');
