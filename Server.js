d056b7d6a62e2b9192fd786f485e7d4b4e17a52b
saif-lesnar <saif_lesnar@outlook.com>
2018-08-19 00:04:09 +0600

    Server.js updated after initializing with chatbot

--- Server.js
+++ Server.js
@@ -7,6 +7,10 @@ var ca = fs.readFileSync( 'encryption/intermediate.crt' );
 var https = require('https');
 var SignupJS = require("./public/js/signup.js");
 var LoginJS =require ("./public/js/login.js");
+//var CheckmoneyJS =require("./public/js/checkmoney.js");
+//var TransactionJS =require ("./public/js/transaction.js");
+//var TestJS = require("./public/js/test.js");
+var ValidationJS =require ("./public/js/validation.js");
 const bodyParser = require('body-parser');
 var options = {
 key: key,
@@ -29,5 +33,22 @@ app.post('/login', function(req, res){
   console.log('hit login');
   LoginJS.login(req, res);
 })
+/*app.post('/transaction', function(req, res){
+  console.log('hit transaction');
+  TransactionJS.transact(req, res);
+})
+/*app.post('/test', function(req, res){
+  console.log('hit transaction');
+  TestJS.validate(req, res);
+})*/
+app.post('/validation', function(req, res){
+  console.log('hit validation');
+  ValidationJS.callAPI(req, res);
+})
+/*app.post('/checkmoney', function(req, res){
+  console.log('hit check mon');
+  CheckmoneyJS.check(req, res);
+})
+*/
 https.createServer(options, app).listen(3000);
 console.log('Server started!');
