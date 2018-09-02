150e9724b01a99f8ecba9192bd54f06e50a3b118
saif-lesnar <saif_lesnar@outlook.com>
2018-09-02 10:15:29 +0600

    user existance checked

--- public/js/signup.js
+++ public/js/signup.js
@@ -34,12 +34,75 @@ function init() {
 	channel.addPeer(peer);
     channel.addOrderer(order);
 }
+module.exports.signupcheck = function(request,res){
+	var user_name = request.body.user_name;
+	var account_no = request.body.account_no;
+	var email = request.body.email;
+	var password = request.body.user_pass;
+	    // create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
+	    Fabric_Client.newDefaultKeyValueStore({ path: store_path
+	    }).then((state_store) => {
+	        // assign the store to the fabric client
+	        fabric_client.setStateStore(state_store);
+	        var crypto_suite = Fabric_Client.newCryptoSuite();
+	        // use the same location for the state store (where the users' certificate are kept)
+	        // and the crypto store (where the users' keys are kept)
+	        var crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
+	        crypto_suite.setCryptoKeyStore(crypto_store);
+	        fabric_client.setCryptoSuite(crypto_suite);
+
+	        // get the enrolled user from persistence, this user will sign all requests
+	        return fabric_client.getUserContext('user1', true);
+	    }).then((user_from_store) => {
+	        if (user_from_store && user_from_store.isEnrolled()) {
+	            console.log('Successfully loaded user1 from persistence');
+	            member_user = user_from_store;
+	        } else {
+	            throw new Error('Failed to get user1.... run registerUser.js');
+	        }
+
+	        // queryCar chaincode function - requires 1 argument, ex: args: ['CAR4'],
+	        // queryAllCars chaincode function - requires no arguments , ex: args: [''],
+
+	        const request = {
+	            //targets : --- letting this default to the peers assigned to the channel
+	            chaincodeId: 'blockchainChatbot',
+	            //fcn: 'queryAllCars',
+	            //args: ['']
+	            fcn: 'login',
+	            //'a0123c'
+	            args: [account_no]
+	        };
+	        console.log(request);
+	        // send the query proposal to the peer
+	        return channel.queryByChaincode(request);
+	    }).then((query_responses) => {
+	        console.log("Query has completed, checking results");
+	        // query_responses could have more than one  results if there multiple peers were used as targets
+	        if (query_responses && query_responses.length == 1) {
+	          if(query_responses[0] != 0){
+							console.log("user exist in the chain");
+							res.send(responseChat);
+						}
+	          else  if (query_responses[0] instanceof Error) {
+	                console.error("error from query = ", query_responses[0]);
+	            }
+	         else {
+	                module.exports.signup(request, res);
+	            }
+	        } else {
+	            console.log("No payloads were returned from query");
+	        }
+	    }).catch((err) => {
+	        console.error('Failed to query successfully :: ' + err);
+	    });
+}
 module.exports.signup = function(request,res){
     var user_name = request.body.user_name;
     var account_no = request.body.account_no;
     var email = request.body.email;
     var password = request.body.user_pass;
-		var money = '10000';
+		var money = '10000000';
     //console.log("Reading: user_name : " +user_name+ " account_no "+account_no+" email "+email+" password "+password);
     // create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
     Fabric_Client.newDefaultKeyValueStore({ path: store_path
